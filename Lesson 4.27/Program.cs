1a7fcf513d92e455d77f19aa202735d1a0445b13
sunny <sunny@DESKTOP-UJ1BUSP>
2023-05-31 22:10:25 +0300

    Добавьте файлы проекта.

---
+++ Lesson 4.27/Program.cs
@@ -0,0 +1,7 @@
+﻿var A = (Console.ReadLine());
+var sum = 0;            
+for (int i = 0; i < A.Length; i++) //смотрим на позицию цифры 
+{
+    sum += Convert.ToInt32 (A[i].ToString()); //берем по одной цифре, преобразуем в Int32 и складываем, так как char при преобразовании выдает id, принудильно преобразовываем в string
+}
+Console.WriteLine(sum);
